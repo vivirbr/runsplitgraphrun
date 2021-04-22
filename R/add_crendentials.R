8e2a63d9d1afa3eec0876ec75b99a53397a5de97
vivirbr <vivian.ribeiro.bio@gmail.com>
2021-04-22 12:24:09 +0200

    keyring for credentials

--- R/add_crendentials.R
+++ R/add_crendentials.R
@@ -1,13 +1,14 @@
-#' Load a Matrix
+#' Writing credentials to the env. variables
 #'
-#' This function downloads the postgresql.jar file directly to your R directory
+#' This function is based on keyring package and writes the credentials to your enviroment
 #'
 #' @param user - splitgraph user from https://www.splitgraph.com/connect
 #' @param password - splitgraph password from https://www.splitgraph.com/connect
+#' @param database - PRIVATE or PUBLIC - default to PUBLIC
 #' @return system variables
 #' @export
 
-add_credentials <- function(user,password){
-        Sys.setenv(SPLITGRAPH_USER = user)
-        Sys.setenv(SPLITGRAPH_PASSWORD = password)
+add_credentials <- function(user,password,database='PUBLIC'){
+        keyring_create(paste0("SPLITGRAPH_",database))
+        key_set_with_value("Splitgraph", user, password = password, keyring = paste0("SPLITGRAPH_",database))
         }
--- R/connect.R
+++ R/connect.R
@@ -2,14 +2,15 @@
 #'
 #' Connect to splitgraph
 #'
+#' @param database - PRIVATE or PUBLIC - default to PUBLIC
 #' @return object dbconnect
 #' @export
 
-connect <- function(){
+connect <- function(database='PUBLIC'){
     conn <- dbConnect(
         JDBC("org.postgresql.Driver", paste0(.libPaths()[1],'/postgresql-42.2.19.jar')),
         "jdbc:postgresql://data.splitgraph.com:5432/ddn",
-        user=Sys.getenv("SPLITGRAPH_USER"),
-        password=Sys.getenv("SPLITGRAPH_PASSWORD")
+        user=key_list(keyring = paste0("SPLITGRAPH_",database))$username,
+        password=key_get("Splitgraph", key_list(keyring = paste0("SPLITGRAPH_",database))$username, keyring = "SPLITGRAPH_PUBLIC")
         )
     }
